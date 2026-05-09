4e1f999268958121e825b64d805fb8f5997f4425
phani-15 <phanipolavarapu15@gmail.com>
2026-05-09 22:29:04 +0530

    git is created for the SIS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div >
+      <h1 className='font-bold text-4xl font-serif'>Hello!!</h1>
+      <h2 className='text-blue-700 text-2xl font-semibold font-serif'>Student Information System</h2>
+    </div>
+  )
+}
